29f22f9f36e4c851bf9e92cb2f24d3a2296cb4f7
guilin <973635949@qq.com>
2017-11-17 16:08:56 +0800

    1

---
+++ demo/src/main/java/a26c/com/android_frame_test/activity/TestAcitivity.java
@@ -0,0 +1,9 @@
+package a26c.com.android_frame_test.activity;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+public class TestAcitivity extends AppCompatActivity {
+
+
+}
--- demo/src/main/java/a26c/com/android_frame_test/activity/TestActivity.java
+++ demo/src/main/java/a26c/com/android_frame_test/activity/TestActivity.java
@@ -1,49 +1,142 @@
 package a26c.com.android_frame_test.activity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import a26c.com.android_frame_test.R;
 
 public class TestActivity extends AppCompatActivity {
 
-
-    private android.view.View view2;
-    private android.view.View emptyView;
-    private ScrollView scrollView;
-
-    GestureDetector gestureDetector;
-    private int firstTopMargin = -1;
+    private android.widget.Button button;
+    private android.widget.FrameLayout sideLayout;
+    private android.support.v4.widget.DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-        this.view2 = (View) findViewById(R.id.view2);
-        this.emptyView = (View) findViewById(R.id.emptyView);
-        this.scrollView = (ScrollView) findViewById(R.id.scrollView);
 
-        view2.post(new Runnable() {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_test_acitivity2);
+
+
+        setStatusBarFullTransparent();
+        setDrawerLayoutFitSystemWindow();
+
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        this.sideLayout = (FrameLayout) findViewById(R.id.sideLayout);
+        this.button = (Button) findViewById(R.id.button);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, view2.getHeight());
-                emptyView.setLayoutParams(layoutParams);
-                scrollView.scrollTo(0,view2.getHeight());
-                System.out.println("height:"+view2.getHeight());
+            public void onClick(View v) {
+                drawerLayout.openDrawer(sideLayout);
             }
         });
 
-
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
+
+    /**
+     * 全透状态栏
+     */
+    protected void setStatusBarFullTransparent() {
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
     }
+
+    /**
+     * 半透明状态栏
+     */
+    protected void setHalfTransparent() {
+
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 如果需要内容紧贴着StatusBar
+     * 应该在对应的xml布局文件中，设置根布局fitsSystemWindows=true。
+     */
+    private View contentViewGroup;
+
+    protected void setFitSystemWindow(boolean fitSystemWindow) {
+        if (contentViewGroup == null) {
+            contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        }
+        contentViewGroup.setFitsSystemWindows(fitSystemWindow);
+    }
+
+    /**
+     * 为了兼容4.4的抽屉布局->透明状态栏
+     */
+    protected void setDrawerLayoutFitSystemWindow() {
+        if (Build.VERSION.SDK_INT == 19) {//19表示4.4
+            int statusBarHeight = getStatusHeight(this);
+            if (contentViewGroup == null) {
+                contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+            }
+            if (contentViewGroup instanceof DrawerLayout) {
+                DrawerLayout drawerLayout = (DrawerLayout) contentViewGroup;
+                drawerLayout.setClipToPadding(true);
+                drawerLayout.setFitsSystemWindows(false);
+                for (int i = 0; i < drawerLayout.getChildCount(); i++) {
+                    View child = drawerLayout.getChildAt(i);
+                    child.setFitsSystemWindows(false);
+                    child.setPadding(0,statusBarHeight, 0, 0);
+                }
+
+            }
+
+
+        }
+    }
+
+    /**
+     * 获得状态栏的高度
+     */
+    public static int getStatusHeight(Context context) {
+
+        int statusHeight = -1;
+        try {
+            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height").get(object).toString());
+            statusHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
+    }
+
 }
