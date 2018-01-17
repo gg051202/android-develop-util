032dcb68538d7c79cd20d8de03c7cafff52b410c
guilin <973635949@qq.com>
2018-01-17 15:36:08 +0800

    ClearEditText修复

--- android-develop-frame/src/main/java/com/a26c/android/frame/widget/ClearEditText.java
+++ android-develop-frame/src/main/java/com/a26c/android/frame/widget/ClearEditText.java
@@ -20,160 +20,159 @@ import com.a26c.android.frame.util.FrameDensityUtils;
  * 带清除功能的EditText
  */
 public class ClearEditText extends EditText implements OnFocusChangeListener, TextWatcher {
-	/**
-	 * 删除按钮的引用
-	 */
-	private Drawable mClearDrawable;
-	/**
-	 * 控件是否有焦点
-	 */
-	private boolean hasFoucs;
-	private Context context;
+    /**
+     * 删除按钮的引用
+     */
+    private Drawable mClearDrawable;
+    /**
+     * 控件是否有焦点
+     */
+    private boolean hasFoucs;
+    private Context context;
 
-	public ClearEditText(Context context) {
-		this(context, null);
-	}
+    public ClearEditText(Context context) {
+        this(context, null);
+    }
 
-	public ClearEditText(Context context, AttributeSet attrs) {
-		// 这里构造方法也很重要，不加这个很多属性不能再XML里面定义
-		this(context, attrs, android.R.attr.editTextStyle);
-	}
+    public ClearEditText(Context context, AttributeSet attrs) {
+        // 这里构造方法也很重要，不加这个很多属性不能再XML里面定义
+        this(context, attrs, android.R.attr.editTextStyle);
+    }
 
-	public ClearEditText(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		this.context = context;
-		init();
-	}
+    public ClearEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+        init();
+    }
 
-	private void init() {
-		setPadding(0,0, FrameDensityUtils.dp2px(context,8),0);
-		// 获取EditText的DrawableRight,假如没有设置我们就使用默认的图片
-		mClearDrawable = getCompoundDrawables()[2];
-		if (mClearDrawable == null) {
-			// throw new
-			// NullPointerException("You can add drawableRight attribute in XML");
-			mClearDrawable = getResources().getDrawable(R.mipmap.frame_icon_delete);
-		}
+    private void init() {
+        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight() + FrameDensityUtils.dp2px(context, 8), getPaddingBottom());
+        // 获取EditText的DrawableRight,假如没有设置我们就使用默认的图片
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            // throw new
+            // NullPointerException("You can add drawableRight attribute in XML");
+            mClearDrawable = getResources().getDrawable(R.mipmap.frame_icon_delete);
+        }
 
-		int width = FrameDensityUtils.dp2px(context, 20);
-		mClearDrawable.setBounds(0, 0, width, width);
-		// 默认设置隐藏图标
-		setClearIconVisible(false);
-		// 设置焦点改变的监听
-		setOnFocusChangeListener(this);
-		// 设置输入框里面内容发生改变的监听
-		addTextChangedListener(this);
-	}
+        int width = FrameDensityUtils.dp2px(context, 20);
+        mClearDrawable.setBounds(0, 0, width, width);
+        // 默认设置隐藏图标
+        setClearIconVisible(false);
+        // 设置焦点改变的监听
+        setOnFocusChangeListener(this);
+        // 设置输入框里面内容发生改变的监听
+        addTextChangedListener(this);
+    }
 
-	/**
-	 * 因为我们不能直接给EditText设置点击事件，所以我们用记住我们按下的位置来模拟点击事件 当我们按下的位置 在 EditText的宽度 -
-	 * 图标到控件右边的间距 - 图标的宽度 和 EditText的宽度 - 图标到控件右边的间距之间我们就算点击了图标，竖直方向就没有考虑
-	 */
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_UP) {
-			if (getCompoundDrawables()[2] != null) {
+    /**
+     * 因为我们不能直接给EditText设置点击事件，所以我们用记住我们按下的位置来模拟点击事件 当我们按下的位置 在 EditText的宽度 -
+     * 图标到控件右边的间距 - 图标的宽度 和 EditText的宽度 - 图标到控件右边的间距之间我们就算点击了图标，竖直方向就没有考虑
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (getCompoundDrawables()[2] != null) {
 
-				boolean touchable = event.getX() > (getWidth() - getTotalPaddingRight()) && (event.getX() < ((getWidth() - getPaddingRight
-						())));
+                boolean touchable = event.getX() > (getWidth() - getTotalPaddingRight()) && (event.getX() < ((getWidth() - getPaddingRight
+                        ())));
 
-				if (touchable) {
-					this.setText("");
-				}
-			}
-		}
+                if (touchable) {
+                    this.setText("");
+                }
+            }
+        }
 
-		return super.onTouchEvent(event);
-	}
+        return super.onTouchEvent(event);
+    }
 
-	/**
-	 * 当ClearEditText焦点发生变化的时候，判断里面字符串长度设置清除图标的显示与隐藏
-	 */
-	@Override
-	public void onFocusChange(View v, boolean hasFocus) {
-		this.hasFoucs = hasFocus;
-		if (hasFocus) {
-			setClearIconVisible(getText().length() > 0);
-		} else {
-			setClearIconVisible(false);
-		}
-	}
+    /**
+     * 当ClearEditText焦点发生变化的时候，判断里面字符串长度设置清除图标的显示与隐藏
+     */
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        this.hasFoucs = hasFocus;
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+    }
 
-	/**
-	 * 设置清除图标的显示与隐藏，调用setCompoundDrawables为EditText绘制上去
-	 *
-	 * @param visible
-	 */
-	protected void setClearIconVisible(boolean visible) {
-		Drawable right = visible ? mClearDrawable : null;
-		setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
-	}
+    /**
+     * 设置清除图标的显示与隐藏，调用setCompoundDrawables为EditText绘制上去
+     *
+     * @param visible
+     */
+    protected void setClearIconVisible(boolean visible) {
+        Drawable right = visible ? mClearDrawable : null;
+        setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
+    }
 
-	/**
-	 * 当输入框里面内容发生变化的时候回调的方法
-	 */
-	@Override
-	public void onTextChanged(CharSequence s, int start, int count, int after) {
-		if (hasFoucs) {
-			setClearIconVisible(s.length() > 0);
-		}
-	}
+    /**
+     * 当输入框里面内容发生变化的时候回调的方法
+     */
+    @Override
+    public void onTextChanged(CharSequence s, int start, int count, int after) {
+        if (hasFoucs) {
+            setClearIconVisible(s.length() > 0);
+        }
+    }
 
-	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-	}
+    }
 
-	@Override
-	public void afterTextChanged(Editable s) {
-		if (onTextChangeListener != null) {
-			onTextChangeListener.afterTextChanged(s);
-		}
-	}
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (onTextChangeListener != null) {
+            onTextChangeListener.afterTextChanged(s);
+        }
+    }
 
-	/**
-	 * 自绘颜色
-	 *
-	 * @Override protected void onDraw(Canvas canvas) { //
-	 *           method stub
-	 *
-	 *           Paint paint = new Paint(); paint.setStyle(Style.STROKE);
-	 *           paint.setStrokeWidth(1); if (this.isFocused() == true) {
-	 *           paint.setColor(getResources().getColor(R.color.ansee)); }else{
-	 *           paint.setColor(getResources().getColor(R.color.ansee)); }
-	 *           canvas.drawRoundRect(new RectF(1+this.getScrollX(),
-	 *           1+this.getScrollY(), this.getWidth()-2+this.getScrollX(),
-	 *           this.getHeight()+this.getScrollY()-1),3,3,paint);
-	 *           super.onDraw(canvas); }
-	 */
-	/**
-	 * 设置晃动动画
-	 */
-	public void setShakeAnimation() {
-		this.setAnimation(shakeAnimation(5));
-	}
+    /**
+     * 自绘颜色
+     *
+     * @Override protected void onDraw(Canvas canvas) { //
+     *           method stub
+     *
+     *           Paint paint = new Paint(); paint.setStyle(Style.STROKE);
+     *           paint.setStrokeWidth(1); if (this.isFocused() == true) {
+     *           paint.setColor(getResources().getColor(R.color.ansee)); }else{
+     *           paint.setColor(getResources().getColor(R.color.ansee)); }
+     *           canvas.drawRoundRect(new RectF(1+this.getScrollX(),
+     *           1+this.getScrollY(), this.getWidth()-2+this.getScrollX(),
+     *           this.getHeight()+this.getScrollY()-1),3,3,paint);
+     *           super.onDraw(canvas); }
+     */
+    /**
+     * 设置晃动动画
+     */
+    public void setShakeAnimation() {
+        this.setAnimation(shakeAnimation(5));
+    }
 
-	/**
-	 * 晃动动画
-	 *
-	 * @param counts
-	 * 		1秒钟晃动多少下
-	 */
-	public static Animation shakeAnimation(int counts) {
-		Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
-		translateAnimation.setInterpolator(new CycleInterpolator(counts));
-		translateAnimation.setDuration(1000);
-		return translateAnimation;
-	}
+    /**
+     * 晃动动画
+     *
+     * @param counts 1秒钟晃动多少下
+     */
+    public static Animation shakeAnimation(int counts) {
+        Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+        translateAnimation.setInterpolator(new CycleInterpolator(counts));
+        translateAnimation.setDuration(1000);
+        return translateAnimation;
+    }
 
-	public interface OnTextChangeListener {
-		void afterTextChanged(Editable s);
-	}
+    public interface OnTextChangeListener {
+        void afterTextChanged(Editable s);
+    }
 
-	public void setOnTextChangeListener(OnTextChangeListener onTextChangeListener) {
-		this.onTextChangeListener = onTextChangeListener;
-	}
+    public void setOnTextChangeListener(OnTextChangeListener onTextChangeListener) {
+        this.onTextChangeListener = onTextChangeListener;
+    }
 
-	private OnTextChangeListener onTextChangeListener;
+    private OnTextChangeListener onTextChangeListener;
 
 }
