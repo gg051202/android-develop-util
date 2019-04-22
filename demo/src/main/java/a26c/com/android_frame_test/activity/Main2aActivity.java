e8ea460b41e232a0256f09b89eca5bca84f913fd
guilin“ <973635949@qq.com>
2019-04-22 17:24:31 +0800

    1

--- demo/src/main/java/a26c/com/android_frame_test/activity/Main2aActivity.java
+++ demo/src/main/java/a26c/com/android_frame_test/activity/Main2aActivity.java
@@ -10,6 +10,6 @@ public class Main2aActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2a);
+        setContentView(R.layout.activity_main);
     }
 }
